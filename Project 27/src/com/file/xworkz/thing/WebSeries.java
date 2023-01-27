ab313fab3948505aeb09c36b2a077de814083c7a
unknown <akshatak.xworkz@gmail.com>
2023-01-27 12:37:45 +0530

    instances

--- Project 27/src/com/file/xworkz/thing/WebSeries.java
+++ Project 27/src/com/file/xworkz/thing/WebSeries.java
@@ -2,7 +2,7 @@ package com.file.xworkz.thing;
 
 public class WebSeries {
 	public WebSeries() {
-		System.out.println("Running in web series");
+		System.out.println("Running in web series...");
 	}
 
 }
