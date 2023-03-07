fda78b056036054fb0a9f10298910f64f8d4fb43
unknown <akshatak.xworkz@gmail.com>
2023-03-07 12:13:33 +0530

    new files

--- Abstraction/src/com/xworkz/abstraction/boot/ATMRunner.java
+++ Abstraction/src/com/xworkz/abstraction/boot/ATMRunner.java
@@ -2,7 +2,7 @@ package com.xworkz.abstraction.boot;
 
 import com.xworkz.abstraction.things.TimeMachine;
 
-public class ATMRunner {
+public class ATMRunner{
 
 	public static void main(String[] args) {
 		TimeMachine timeMachine =new TimeMachine();
---
+++ Project 27/src/com/file/xworkz/technical/Num.java
@@ -0,0 +1,10 @@
+package com.file.xworkz.technical;
+
+public class Num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
